a202242cbbd050cbb1b824afcbf7dfbe825e1d36
Konex25 <rapacz.konrad@gmail.com>
2025-12-17 15:46:38 +0100

    Treatment class

---
+++ src/Treatment.java
@@ -0,0 +1,15 @@
+public class Treatment {
+    private String name;
+
+    public Treatment(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

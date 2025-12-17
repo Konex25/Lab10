5f0f16753cc788983176266526f84f6fe3e741a9
Konex25 <rapacz.konrad@gmail.com>
2025-12-17 15:56:34 +0100

    Appointment class

---
+++ src/Appointment.java
@@ -0,0 +1,29 @@
+import java.time.LocalDateTime;
+
+public class Appointment {
+    private LocalDateTime dateTime;
+    private Patient patient;
+    private Doctor doctor;
+
+    public Appointment(LocalDateTime dateTime, Patient patient, Doctor doctor) {
+        this.dateTime = dateTime;
+        this.patient = patient;
+        this.doctor = doctor;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+}
